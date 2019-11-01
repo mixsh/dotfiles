771b262966f0f4e897e7ea78c47f8ee76da7370e
Michael Siegwarth <msiegwarth@gmail.com>
2019-11-01 14:14:31 -0300

    First polybar

--- link.sh
+++ link.sh
@@ -3,6 +3,7 @@ mkdir -p ~/.config/i3status
 mkdir -p ~/.config/rofi/themes
 mkdir -p ~/.config/compton
 mkdir -p ~/.config/surfraw
+mkdir -p ~/.config/polybar
 
 cd ~/ && rm -f ~/.vimrc && ln -s ~/.dotfiles/.vimrc .vimrc
 cd ~/ && rm -f .Xresources && ln -s ~/.dotfiles/.Xresources .Xresources
@@ -13,6 +14,7 @@ cd ~/.config && rm -f compton.conf && ln -s ~/.dotfiles/.config/compton.conf com
 cd ~/.config/i3 && rm -f config && ln -s ~/.dotfiles/.config/i3/config config
 cd ~/.config/i3status && rm -f config && ln -s ~/.dotfiles/.config/i3status/config config
 cd ~/.config/surfraw && rm -f conf && ln -s ~/.dotfiles/.config/surfraw/conf conf
+cd ~/.config/polybar && rm -f config && ln -s ~/.dotfiles/.config/polybar/config config
 
 cd ~/.config/rofi/themes && rm -f mixsh.config && rm -f mixsh.rasi && ln -s ~/.dotfiles/.config/rofi/themes/mixsh.config mixsh.config && ln -s ~/.dotfiles/.config/rofi/themes/mixsh.rasi mixsh.rasi
 
