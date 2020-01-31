553200d6fa73dc7852262a415796f69c52a86e94
Michael Siegwarth <msiegwarth@gmail.com>
2020-01-30 22:52:36 -0300

    grayscale lock

--- scripts/lock.sh
+++ scripts/lock.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm /tmp/screen.png
+rm -f /tmp/screen.png
 scrot /tmp/screen.png
-convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
+convert /tmp/screen.png -scale 10% -scale 1000% -colorspace Gray -gamma 0.9 /tmp/screen.png
 
 # dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player.Stop
 
