b6e5f43ca633c7833cc1c9da5006323a606ea859
Michael Siegwarth <msiegwarth@gmail.com>
2020-01-30 22:37:05 -0300

    Added imagemagick

--- scripts/install.sh
+++ scripts/install.sh
@@ -43,6 +43,7 @@ sudo apt install -y mpd
 sudo apt install -y htop
 sudo apt install -y lightdm-gtk-greeter lightdm-gtk-greeter-settings
 sudo apt install -y lightdm
+sudo apt install -y imagemagick
 
 # polybar
 sudo apt-get install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2 -y && git clone https://github.com/jaagr/polybar.git && cd polybar && sudo ./build.sh
