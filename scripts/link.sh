aca222ea2cfea7bddce04fef5365d78a02a8a524
Michael <michael@pop-os.localdomain>
2020-05-20 13:52:22 -0300

    Script improvements

--- scripts/link.sh
+++ scripts/link.sh
@@ -1,4 +1,4 @@
 cd ~/ && rm -f ~/.vimrc && ln -s ~/.dotfiles/.vimrc .vimrc
-cd ~/ && rm -f .Xresources && ln -s ~/.dotfiles/.Xresources .Xresources
-cd ~/ && rm -f .zshrc && ln -s ~/.dotfiles/.zshrc .zshrc
-cd ~/ && rm -f .profile && ln -s ~/.dotfiles/.profile .profile
+cd ~/ && rm -f ~/.Xresources && ln -s ~/.dotfiles/.Xresources .Xresources
+cd ~/ && rm -f ~/.zshrc && ln -s ~/.dotfiles/.zshrc .zshrc
+cd ~/ && rm -f ~/.profile && ln -s ~/.dotfiles/.profile .profile
